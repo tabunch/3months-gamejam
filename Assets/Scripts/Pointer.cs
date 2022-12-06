4d8d29f929a9ed0d563214daa85642cacd0389e5
yosizuka <76939507+yosizuka@users.noreply.github.com>
2022-12-06 20:24:03 +0900

    コピーしたオブジェクトのアイコンをUI上に表示するように変更

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pointer : MonoBehaviour
 {
     private Vector2 mousePos;
     private Vector2 pointerPos;
     public GameObject target;
+    [SerializeField] GameObject copiedObject;
+    Image copiedObjectIcon;
 
     // Start is called before the first frame update
     void Start()
     {
         target = null;
+        copiedObjectIcon = copiedObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -25,6 +29,8 @@ public class Pointer : MonoBehaviour
     private void OnTriggerStay2D(Collider2D other) {
         if(Input.GetMouseButton(0) && other.gameObject.CompareTag("Copyable")){
             target = other.gameObject;
+            copiedObjectIcon.sprite = target.GetComponent<SpriteRenderer>().sprite;
+            copiedObjectIcon.color = new Color(255.0f/255.0f, 255.0f/255.0f, 255.0f/255.0f, 255.0f/255.0f);
         }
     }
 }
