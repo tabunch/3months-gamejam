6c868e878f88a3d833cb30e26349ac295ea65ca8
yosizuka <76939507+yosizuka@users.noreply.github.com>
2023-01-11 21:12:24 +0900

    ファイルを移動，アセットを追加，シナリオ用プログラムを一部変更

---
+++ Assets/Main/Scripts/ScenarioManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioManager : SingletonMonoBehaviour<ScenarioManager>
+{
+    [SerializeField] TextAsset scenarioText;
+    string[] scenarios;
+    string delimiter;
+    [SerializeField] GameObject MessageWindow;
+    MessageManager messageManager;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        delimiter = "\n\n";
+        messageManager = MessageWindow.GetComponent<MessageManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScenarioFile(string fileName){
+        // scenarioText = Resources.Load<TextAsset>("Scenarios/" + fileName);
+        if(scenarioText == null){
+            Debug.LogError("シナリオファイルが見つかりません");
+            return;
+        }
+        scenarios = scenarioText.text.Split(delimiter);
+        messageManager.ActivateMessagePanel(scenarios);
+        messageManager.NextLine();
+    }
+
+    public bool IsScenarioFinished(){
+        return messageManager.isMessageFinished;
+    }
+}
